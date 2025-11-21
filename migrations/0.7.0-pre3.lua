200d760b4a226b79ad7c6bf6db4de07732748bb4
PizzaPlanner <80210845+PizzaPlanner@users.noreply.github.com>
2025-11-21 20:21:11 +0100

    whoops

--- migrations/0.7.0-pre3.lua
+++
@@ -1,5 +0,0 @@
-local rabbasca = game.surfaces["rabbasca"]
-if not rabbasca then return end 
-
-rabbasca.regenerate_entity({ "rabbasca-vault-crafter", "rabbascan-scrap" })
-game.print("[space-location=rabbasca] Update 0.7 altered a some recipes, with notable changes to [entity=rabbascan-scrap] and [item=calcite]/[item=stone]/[item=advanced-circuit] acquisition. Make sure to check your factory on Rabbasca and the mod's changelog.")
