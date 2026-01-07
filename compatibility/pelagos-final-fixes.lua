e95abc5a7a6e54dcf33a6ffe9926efc1dd9678ef
PizzaPlanner <80210845+PizzaPlanner@users.noreply.github.com>
2026-01-07 05:02:51 +0100

    fixed pelagos barrels not doing anything

--- compatibility/pelagos-final-fixes.lua
+++ compatibility/pelagos-final-fixes.lua
@@ -1,7 +1,7 @@
 local carotene_barrel = data.raw["item"]["beta-carotene-barrel"]
 local carotene_barrel_2 = data.raw["item"]["beta-carotene-titanium-barrel"]
 
-if carotene_barrel and carotene_barel_2 then
+if carotene_barrel and carotene_barrel_2 then
     carotene_barrel_2.fuel_category = carotene_barrel.fuel_category
     carotene_barrel_2.fuel_value = carotene_barrel.fuel_value
 end
@@ -9,7 +9,7 @@ end
 local carotene_barrel = data.raw["item"]["omega-carotene-barrel"]
 local carotene_barrel_2 = data.raw["item"]["omega-carotene-titanium-barrel"]
 
-if carotene_barrel and carotene_barel_2 then
+if carotene_barrel and carotene_barrel_2 then
     carotene_barrel_2.fuel_category = carotene_barrel.fuel_category
     carotene_barrel_2.fuel_value = carotene_barrel.fuel_value
 end
