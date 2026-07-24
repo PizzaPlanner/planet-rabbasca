e5e6de20151827ba4ddbe77cc43c8a3b5308b407
PizzaPlanner <80210845+PizzaPlanner@users.noreply.github.com>
2026-07-24 03:29:39 +0200

    more k2so compat

--- compatibility/k2so-final-fixes.lua
+++ compatibility/k2so-final-fixes.lua
@@ -18,3 +18,44 @@ if data.raw["ammo"]["kr-anti-materiel-rifle-magazine"] then
         { type = "item", name = "kr-anti-materiel-rifle-magazine", amount = 1, independent_probability = 0.08 }
     )
 end
+
+if data.raw["item"]["kr-coke"] then
+    data:extend {
+        {
+            type = "recipe",
+            name = "rabbasca-kr-coke-carotenoid",
+            icons = Rabbasca.icons({
+                { proto = data.raw["item"]["kr-coke"] },
+                { proto = data.raw["item"]["carotenoid-ore"], scale = 0.5, shift = {-4, -4} },
+            }),
+            enabled = false,
+            energy_required = 16,
+            ingredients = {
+                { type = "item", name = "carotenoid-ore", amount = 6 },
+                { type = "item", name = "carbon", amount = 6 },
+            },
+            results = { 
+                { type = "item", name = "kr-coke", amount = 6 },
+            },
+            allow_productivity = true,
+            categories = { "smelting" },
+            order = "b[chemistry]-g[coke]"
+        }
+    }
+    table.insert(data.raw["technology"]["carotene"].effects, { type = "unlock-recipe", recipe = "rabbasca-kr-coke-carotenoid" })
+end
+
+if data.raw["item"]["kr-rare-metal-ore"] then
+    Rabbasca.create_vault_recipe("vault-protocol-kr-rare-metal-ore", {
+        icons = Rabbasca.icons({
+            {icon = "__Krastorio2Assets__/icons/entities/stabilizer-charging-station.png", icon_size = 64},
+            {proto = data.raw["item"]["kr-rare-metal-ore"], shift = { 4, 4 }, scale = 0.75},
+        }),
+        results = {
+            {type = "item", name = "kr-rare-metal-ore", amount = 25 },
+        }, 
+        energy_required = 28,
+        allow_productivity = true,
+    })
+    table.insert(data.raw["technology"]["rabbascan-vault-access"].effects, { type = "unlock-recipe", recipe = "vault-protocol-kr-rare-metal-ore" })
+end
