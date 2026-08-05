15d5be56172a2694c50626d8e8176515dbb11e28
PizzaPlanner <80210845+PizzaPlanner@users.noreply.github.com>
2026-08-05 23:37:10 +0200

    factorio 2.1.13 update

--- prototypes/ambient-sounds.lua
+++ prototypes/ambient-sounds.lua
@@ -3,7 +3,7 @@ data:extend{
     type = "ambient-sound",
     name = "rabbasca-silence",
     title = "Rabbasca Silence",
-    planet = "rabbasca",
+    planets = { "rabbasca" },
     track_type = "main-track",
     sound = {filename = "__space-age__/sound/ambient/space/space-2.ogg", volume = 0,},
     weight = 10
